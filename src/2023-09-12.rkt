fdf465a52746407730d26a95620cd5363b9adacc
Hiroshi Kimura <hiroshi.kimura.0331@gmail.com>
2023-09-12 13:06:34 +0900

    added 2023-09-12 documents. rember2-aux etc.

--- src/2023-09-12.rkt
+++ src/2023-09-12.rkt
@@ -2,25 +2,49 @@
 
 (require "00-atom.rkt")
 
-(define length2-aux
-  (lambda (l n)
+;; ごく自然な再帰で。
+(define length
+  (lambda (lst)
     (cond
-      ((null? l) n)
-      (else (length2-aux (rest l) (+ 1 n))))))
+      ((null? lst) 0)
+      (else (+ 1 (length (rest lst)))))))
+
+;; 補助関数 length2-aux を導入、同時に補助変数 n も導入する。
+;; 補助変数は戻り値予定のもの。
+(define length2-aux
+  (lambda (lst n)
+    (cond
+      ((null? lst) n)
+      (else (length2-aux (rest lst) (+ 1 n))))))
 
 (define length2
-  (lambda (l)
-    (length2-aux l 0)))
+  (lambda (lst)
+    (length2-aux lst 0)))
 
-; (length2 '(1 2 3 3 2))
+;; リスト lst から最初に見つかるアトム a を除いたリストを返す。
+(define rember
+  (lambda (a lst)
+    (cond
+      ((null? lst) '())
+      ((eq? (first lst) a) (rest lst))
+      (else (cons (first lst)
+                  (rember a  (rest lst)))))))
 
-(define rember-aux
+; (rember '3 '(1 2 3 4 5))
+
+;; 補助関数 rember2-aux の導入。補助変数 ret に戻りリストを作る。
+;; 戻りリストの要素順がちょっと気になるが。。。
+(define rember2-aux
   (lambda (a lst ret)
     (cond
       ((null? lst) ret)
-      ((eq? (first lst) a) (append (reverse (rest lst)) ret))
-      (else (rember-aux a (rest lst) (cons (first lst) ret))))))
+      ((eq? (first lst) a) (append (rest lst) ret))
+      (else (rember2-aux a (rest lst) (cons (first lst) ret))))))
 
-(define rember
+(define rember2
   (lambda (a lst)
-    (reverse (rember-aux a lst '()))))
+    (rember2-aux a lst '())))
+
+; (rember2 1 '(1 2 3 4 5))
+; (rember2 3 '(1 2 3 4 5))
+; (rember2 5 '(1 2 3 4 5))
